92c792936639609d10e3a769e548c19ac8ee340a
Prathmesh Jakkanwar <prathmesh.jakkanwar@gmail.com>
2018-11-15 01:56:13 -0500

    Final working

--- js/enroll.js
+++ js/enroll.js
@@ -1,182 +1,3 @@
-// (function($) {
-//     $(document).ready(function() {
-//        console.log("Ready to submit");
-
-
-//         // Get the modal
-//         var modal = document.getElementById('registrationModal');
-
-//         // Get the button that opens the modal
-//         var btn = document.getElementById("registrationButton");
-
-//         console.log("THIS IS INIT");
-//         // Get the button that opens the modal
-//         var btn_lg = $('[data-toggle-modal="enroll"]');
-
-//         // Get the <span> element that closes the modal
-//         var span = document.getElementsByClassName("close")[0];
-
-//         // When the user clicks on the button, open the modal
-//         btn.onclick = function() {
-//             //modal.style.display = "block";
-
-
-//             $(modal).fadeIn();
-//         }
-
-//         $(btn_lg).click(function(){
-//             $(modal).fadeIn();
-//         });
-//         // btn_lg.onclick = function() {
-//         //     console.log("CLICKED");
-//         //     //modal.style.display = "block";
-//         //     $(modal).fadeIn();
-//         // }
-
-//         // When the user clicks on <span> (x), close the modal
-//         span.onclick = function() {
-//             $(modal).fadeOut();
-//             //modal.style.display = "none";
-//         }
-
-//         // When the user clicks anywhere outside of the modal, close it
-//         window.onclick = function(event) {
-//             if (event.target == modal) {
-//                 //modal.style.display = "none";
-//                 $(modal).fadeOut();
-//             }
-//         }
-
-
-//         // $('#submit-form').on('click', function(e) {
-//         //     e.preventDefault();
-//         //     enroll();
-//         //
-//         // })
-
-//         $('#gform').submit(function(e){
-//             e.preventDefault();
-
-
-
-//             swal("Are you enrolling from within the European Union?", {
-//                 buttons: {
-//                     gdpr: "Yes",
-//                     nongdpr: "No",
-//                     cancel: true,
-//                 },
-//             }).then((value) => {
-//                 switch (value) {
-
-//                 case "gdpr":
-//                     enroll("EU");
-//                     break;
-//                 case "nongdpr":
-//                     enroll("US");
-//                     break;
-//                 default:
-//                     $(modal).fadeOut();
-//                     break;
-//                 }
-//             });
-
-
-//             // EU - https://script.google.com/macros/s/AKfycbwmjYe6PNQgT8rPjQGRaRhjMS5TYbIU9WLn5oLJ/exec
-//             // US - https://script.google.com/macros/s/AKfycbz6VNJ7Y8sWlcoOiTG2nl2Hd35IgvVy79VWcG_k/exec
-
-
-//         });
-
-
-//     });
-// })(jQuery);
-
-
-// function enroll(data)
-// {
-//     var modal = document.getElementById('registrationModal');
-//     if(data=="EU")
-//     {
-//         $.ajax({
-//             url:'https://script.google.com/macros/s/AKfycbwmjYe6PNQgT8rPjQGRaRhjMS5TYbIU9WLn5oLJ/exec',
-//             type:'POST',
-//             data:$('#gform').serialize(),
-//             success:function(){
-//                 swal("Thank you!", "We'll keep you posted on the upcoming registrations", "success");
-//                 $("input.form-control").val("");
-//                 $(modal).fadeOut();
-//             }
-//         });
-
-//     }
-//     else if(data=="US")
-//     {
-//         $.ajax({
-//             url:'https://script.google.com/macros/s/AKfycbz6VNJ7Y8sWlcoOiTG2nl2Hd35IgvVy79VWcG_k/exec',
-//             type:'POST',
-//             data:$('#gform').serialize(),
-//             success:function(){
-//                 swal("Thank you!", "We'll keep you posted on the upcoming registrations", "success");
-//                 $("input.form-control").val("");
-//                 $(modal).fadeOut();
-//             }
-//         });
-//     }
-
-
-// }
-
-// $(".registrationSubmit").click(function(e){
-// e.preventDefault;
-//     var name = $("#firstName").val();
-//     var action = $("#applicationForm").attr('action');
-
-// swal("Are you enrolling from within the European Union?", {
-//     buttons: {
-//         gdpr: "Yes",
-//         nongdpr: "No",
-//         cancel: true,
-//     },
-// }).then((value) => {
-//     switch (value) {
-
-//     case "gdpr":
-//         //EU
-//         action = "https://script.google.com/macros/s/AKfycbwgAg3ZbuLvDsffaYTzSvjFkVx-JJcduK9HbCMtVKxk2FlIXO72/exec";
-//         break;
-//     case "nongdpr":
-//         //US
-//         action = "https://script.google.com/macros/s/AKfycbzDxZJjZSrxmmChAfu24sT6H5wsvknKu4G8ImIl4XnNrtU0kWlR/exec";
-//         break;
-//     default:
-//         $(modal).fadeOut();
-//         break;
-//     }
-
-
-//     // console.log("action: " + action);
-
-
-
-//     $.ajax({
-//         url: action,
-//         type:'POST',
-//         data:$('#applicationForm').serialize(),
-//         success:function(){
-//             $("#registration-hackathon").fadeOut();
-//             $("#registration-thankyou").fadeIn();
-//         }
-//     });
-
-
-
-
-
-// });
-
-// });
-
-
 
 function UploadRelease() {
 
@@ -218,28 +39,27 @@ function UploadRelease() {
                 console.log("result: " + response.result);
                 console.log("data: " + response.data);
 
+                if(response.result == 'fail' && response.data == 'Invalid email for registration') {
+                    swal("Error!", "Email you entered did not match your application email", "error");
+                } else {
+                    swal("Nice!", "Your enrollment has been successfully submitted", "success");
 
-                swal("Nice!", "Your enrollment has been successfully submitted", "success");
+                    $("#releaseDriveLocation").attr("value",response.data);
+                    $("#resume").fadeOut();
 
 
-                $("#releaseDriveLocation").attr("value",response.data);
-                $("#resume").fadeOut();
+                    $("#SelectedRelease").fadeOut();
+                    $("#resumeWarning").fadeOut();
+                    $("#SelectedRelease").fadeOut();
 
+                    $("#loadRelease").attr("background","#4cae4c");
+                    $("#loadRelease").attr("disabled",true);
+                    $("#loadRelease").css("background","green");
+                    $("#loadRelease").css("color","white");
+                    $("#loadRelease").fadeOut();
 
-                $("#SelectedRelease").fadeOut();
-                $("#resumeWarning").fadeOut();
-                $("#SelectedRelease").fadeOut();
-
-                $("#loadRelease").attr("background","#4cae4c");
-                $("#loadRelease").attr("disabled",true);
-                $("#loadRelease").css("background","green");
-                $("#loadRelease").css("color","white");
-                $("#loadRelease").fadeOut();
-
-                // $("#registrationSubmit").show();
-                $(".release_submitted").show();
-                // $("#registrationSubmit").attr("disabled",false);
-                // $("#registrationSubmit").toggleClass("btn-primary");
+                    $(".release_submitted").show();
+                }
             }
 
         }
@@ -247,3 +67,21 @@ function UploadRelease() {
     };
     reader.readAsDataURL(file);
 }
+
+
+$(".releaseSubmit").click(function(e){
+    e.preventDefault;
+        var name = $("#firstName").val();
+        var action = $("#enrollmentForm").attr('action');
+
+        $.ajax({
+            url: "https://script.google.com/macros/s/AKfycbyVPtaXDqPc4xNChZLcY-jsS9Mzi-7g3p_YfrDNqKx32MmnM861/exec",
+            type:'POST',
+            data:$('#enrollmentForm').serialize(),
+            success:function(){
+                $("#enrollment-hackathon").fadeOut();
+                $("#enrollment-thankyou").fadeIn();
+            }
+        });
+    
+});
